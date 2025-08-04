24c69c50aabee620821f5626157811a875fd560d
Suman Regmi <limitlessplaying@gmail.com>
2025-08-04 21:44:46 +0545

    Task api completed

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -3,6 +3,7 @@ from .models import Task
 from projects.serializers import ProjectSerializer
 
 class TaskSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(min_length=5, max_length=10)
     user = serializers.HyperlinkedRelatedField(read_only = True, many=False, view_name = 'user-detail')
     assigned_to = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name = "user-detail")
     project = ProjectSerializer(read_only = True)
