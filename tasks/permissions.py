f646bd8d9c4391013c551c97357fabff4ed92f94
Suman Regmi <limitlessplaying@gmail.com>
2025-08-10 21:26:20 +0545

    task permission and user serializer fixing

---
+++ tasks/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+class IsTaskOwnerOrReadOnly(permissions.BasePermission):
+    """Custom permission to only allow owners of a task or read-only access."""
+    
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        if request.user in obj.assigned_to.all():
+            return request.method in permissions.SAFE_METHODS
+        
+        if obj.user == request.user:
+            return True
+        
+        # if not request.user.is_anonymous:
+        #     return request.user == obj.user
+        
+        return False
--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Profile
 from django.contrib.auth.models import User
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name='user-detail')
     class Meta:
@@ -11,6 +10,29 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only = True)
+    password = serializers.CharField(write_only = True, required = False)
+    
+    def create(self, validated_data):
+        try:
+            password = validated_data.pop('password')
+            user = User.objects.create(**validated_data)
+            user.set_password(password)
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError({'error': str(err)})
+        
+        return user
+    
+    def update( self, instance, validated_data):
+        try:
+            user = instance
+            if 'password' in validated_data:
+                password = validated_data.pop('password')
+                user.set_password(password)
+                user.save()
+        except Exception as err:
+            raise serializers.ValidationError({'error': str(err)})
+            
     class Meta:
         model = User
         fields = ['id','url', 'username','first_name','last_name','username', 'email', 'password', 'profile']
